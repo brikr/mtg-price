ac23dbe0179c8e663419ca8584d9e20bc996cccf
Brik Royster <elkemore@gmail.com>
2017-03-10 22:56:28 -0700

    good card data, lambda function checks cache now

--- mtg-price-server/handler.py
+++ mtg-price-server/handler.py
@@ -1,20 +1,44 @@
+import boto3
+import decimal
 import json
+from boto3.dynamodb.conditions import Key, Attr
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
 
 def card(event, context):
+    cards = json.load(open('cards.json'))
     search = event['pathParameters']['name']
 
-    response = table.scan(
-        FilterExpression=Key('name').begins_with(search)
-    )
+    matches = dict((k, v) for k, v in cards.items() if k.lower().startswith(search.lower()))
+
+    table = boto3.resource('dynamodb').Table('mtg_price_cards')
+
+    results = list()
+
+    for name, sets in matches.iteritems():
+        response = table.query(
+            KeyConditionExpression=Key('name').eq(name)
+        )
+        results.extend(response[u'Items'])
 
     body = {
-        'error': false,
-        'matches': response[u'Items']
+        'error': False,
+        'cached': results,
+        'matches': matches,
+        'event': event
     }
 
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps(body, cls=DecimalEncoder)
     }
 
     return response
--- mtg-price-server/scratch.py
+++ mtg-price-server/scratch.py
@@ -4,26 +4,43 @@
 
 import boto3
 import decimal
+import json
 from boto3.dynamodb.conditions import Key, Attr
+
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            if o % 1 > 0:
+                return float(o)
+            else:
+                return int(o)
+        return super(DecimalEncoder, self).default(o)
+
+search = 'tar'
+
+cards = json.load(open('cards.json'))
+matches = dict((k, v) for k, v in cards.items() if k.lower().startswith(search.lower()))
+
+print(matches)
 
 table = boto3.resource('dynamodb').Table('mtg_price_cards')
 
-search = 'Tarmo'
+results = list()
 
-response = table.scan(
-    FilterExpression=Key('name').begins_with(search)
-)
+for name, sets in matches.iteritems():
+    response = table.query(
+        KeyConditionExpression=Key('name').eq(name)
+    )
+    results.extend(response[u'Items'])
 
-print(response)
+print(json.dumps(results, cls=DecimalEncoder))
 
-response = table.put_item(
-    Item={
-        'name': 'Tarmogoyf',
-        'set': 'FUT',
-        'paper': decimal.Decimal('169.99'),
-        'online': decimal.Decimal('100.40'),
-        'time': 1489170621
-    }
-)
-
-print(response)
+# response = table.put_item(
+#     Item={
+#         'name': 'Tarmogoyf',
+#         'set': 'MM3',
+#         'paper': decimal.Decimal('96.99'),
+#         'online': decimal.Decimal('70.40'),
+#         'time': 1489170621
+#     }
+# )
